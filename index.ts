74d1b619ea5fe937c03b11eb5457d9f810cc7624
Vivek Raj <vivekrajr@gmail.com>
2017-01-28 13:24:00 +0100

    Adds functionality to add a todo

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
 let root = new Vue({
-  el: '#root',
-  data: {
-    todos: [
-      {text: 'Go shopping', completed: false},
-      {text: 'Give', completed: false},
-      {text: 'Clean Room', completed: false},
-      {text: 'Dishes', completed: false},
-      {text: 'Vacuum', completed: true},
-      {text: 'Yoga', completed: false}
-    ]
-  },
-  computed: {
-    completedTasks() {
-      return this.todos.filter(item => item.completed);
+    el: '#root',
+    data: {
+        todos: [
+            { text: 'Go shopping', completed: false },
+            { text: 'Give', completed: false },
+            { text: 'Clean Room', completed: false },
+            { text: 'Dishes', completed: false },
+            { text: 'Vacuum', completed: true },
+            { text: 'Yoga', completed: false }
+        ]
     },
-    pendingTasks() {
-      return this.todos.filter(item => ! item.completed);
+    computed: {
+        completedTasks() {
+            return this.todos.filter(item => item.completed);
+        },
+        pendingTasks() {
+            return this.todos.filter(item => !item.completed);
+        }
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({ text: this.newTodoText, completed: false });
+            this.newTodoText = '';
+        }
     }
-  }
 });
