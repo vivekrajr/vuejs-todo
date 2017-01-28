b5b5a4a062dcf6d1b68b25b00858da79adda0029
Vivek Raj <vivekrajr@gmail.com>
2017-01-28 13:00:28 +0100

    Adds a barebones todo app

---
+++ index.ts
@@ -0,0 +1,21 @@
+let root = new Vue({
+  el: '#root',
+  data: {
+    todos: [
+      {text: 'Go shopping', completed: false},
+      {text: 'Give', completed: false},
+      {text: 'Clean Room', completed: false},
+      {text: 'Dishes', completed: false},
+      {text: 'Vacuum', completed: true},
+      {text: 'Yoga', completed: false}
+    ]
+  },
+  computed: {
+    completedTasks() {
+      return this.todos.filter(item => item.completed);
+    },
+    pendingTasks() {
+      return this.todos.filter(item => ! item.completed);
+    }
+  }
+});
